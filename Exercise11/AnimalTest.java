488f3328b70fbb0b4a68f8a2cd610c6c5846da36
Yung Han Khoe <yung.khoe@student.uva.nl>
2015-09-04 18:47:12 +0200

    Adding Exercise 11

---
+++ Exercise11/AnimalTest.java
@@ -0,0 +1,95 @@
+class Animal {
+
+   // Properties of the class...
+   public int     numberOfLegs;
+   public boolean hasWings;
+
+   // Constructor of the class...
+   public Animal() {
+      numberOfLegs = 4;
+      hasWings = false;
+   }
+
+   // Methods of the class...
+   public void talk() {
+      System.out.println("Hello");
+   }
+}
+
+class Bird extends Animal {
+
+   // Properties of the class...
+   public boolean canFly;
+
+   // Constructor of the class...
+   public Bird() {
+      numberOfLegs = 2;
+      hasWings = true;
+      canFly = true;
+   }
+
+   // Methods of the class...
+   public void fly() {
+      System.out.println("flap flap");
+   }
+}
+
+class Eagle extends Bird {
+
+   // Properties of the class...
+   private int numberOfKills;
+
+   // Constructor of the class...
+   Eagle() {
+      numberOfKills = 0;
+   }
+
+   // Methods of the class...
+   public void attack() {
+      numberOfKills++;
+   }
+}
+
+class AnimalTest {
+
+   // The main method is the point of entry into the program...
+   public static void main(String[] args) {
+
+      Animal a = new Animal();
+      System.out.println(a.numberOfLegs);
+      System.out.println(a.hasWings);
+      a.talk();
+      
+      // Animal has no fly method
+      //a.fly();
+
+      Bird b = new Bird();
+      System.out.println(b.numberOfLegs);
+      System.out.println(b.hasWings);
+      System.out.println(b.canFly);
+      
+      // Bird has no numberOfKills property
+      //System.out.println(b.numberOfKills);
+      b.talk();
+      
+      // Bird has no attack method
+      //b.attack(); 
+
+      Eagle e = new Eagle();
+      
+      // numberOfKills is a private property
+      //System.out.println(e.numberOfKills);
+      System.out.println(e.numberOfLegs);
+      System.out.println(e.hasWings);
+      e.talk();
+      e.attack();
+
+        a = b;
+	a.talk();
+	//a.fly();
+
+	b = a;
+	b.talk();
+	b.fly();
+   }
+}
